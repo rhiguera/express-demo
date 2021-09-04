c7fae5622acf6886b06d766cd8785d8b10652206
rhiguera <rhiguera75@gmail.com>
2021-09-04 17:40:48 +0200

    Add project to git

---
+++ test/posts.spec.js
@@ -0,0 +1,14 @@
+const request = require('supertest')
+const app = require('../src/app') // the express server
+const expect = require('chai').expect
+
+describe('GET /posts endpoint', () => {
+    it('GET /posts endpoint successfully returns response', () => {
+        return request(app)
+            .get('/posts')
+            .then((response) => {
+                expect(response.statusCode).to.equal(200)
+                expect(response.text).to.equal('You have hit GET /posts endpoint rafa')
+            })
+    })
+})
